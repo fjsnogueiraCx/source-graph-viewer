8c5ee71e9931a9abe24da251d609fdb4b6ecdc98
Michael Gumowski <michael.gumowski@sonarsource.com>
2017-09-15 10:25:15 +0200

    Setup Travis CI (#5)

---
+++ travis.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+set -euo pipefail
+
+function configureTravis { 
+  mkdir ~/.local
+  curl -sSL https://github.com/SonarSource/travis-utils/tarball/v36 | tar zx --strip-components 1 -C ~/.local
+  source ~/.local/bin/install
+}
+configureTravis
+
+. installJDK8
+
+# CI: build (incl. tests), deploy artifacts and triggers NEXT analysis (incl. JaCoCo)
+regular_mvn_build_deploy_analyze
