ad8ea422dd7f7715a2cf0c144766501ceed35063
Michael Gumowski <michael.gumowski@sonarsource.com>
2017-09-15 11:16:18 +0200

    fix travis.sh

--- travis.sh
+++ travis.sh
@@ -2,7 +2,7 @@
 set -euo pipefail
 
 function configureTravis { 
-  mkdir ~/.local
+  mkdir -p ~/.local
   curl -sSL https://github.com/SonarSource/travis-utils/tarball/v36 | tar zx --strip-components 1 -C ~/.local
   source ~/.local/bin/install
 }
